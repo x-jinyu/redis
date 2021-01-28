619aefebcf6d50181ce6fc6535b13f324829b562
sublun <sublun@126.com>
2021-01-28 11:12:04 +0800

    第二次

--- Web项目/JavaProjects/Day14/src/demo2/Brid.java
+++
@@ -1,9 +0,0 @@
-package demo2;
-
-/**
- * 王金宇
- * 2020/10/12 0012
- */
-public abstract class Brid {
-    public abstract void fly();
-}
--- Web项目/JavaProjects/Day14/src/demo3/Fly.java
+++
@@ -1,9 +0,0 @@
-package demo3;
-
-/**
- * 王金宇
- * 2020/10/12 0012
- */
-public interface Fly {
-    void fly();
-}
